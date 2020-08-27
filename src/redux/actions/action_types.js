cfc6f4f2fff7b4fb22dab67a2d0c6a2f77871a83
anurag-gothi-au6 <advisoranurag@gmail.com>
2020-08-27 15:02:09 +0530

    fix

--- src/redux/actions/action_types.js
+++
@@ -1,4 +0,0 @@
-export const fetch_user_data = 'fetch_user_data'
-export const fetch_user_newsfeed = 'fetch_user_newsfeeds'
-export const fetch_post = 'fetch_post'
-export const fetch_user_private_post ='fetch_user_private_post'
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import reducer from './reducers/root_reducter'
-import thunk from 'redux-thunk'
-
-
-let store = createStore(reducer,composeWithDevTools(applyMiddleware(thunk)))
-
-export default store;
